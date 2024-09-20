dc9e3a509296e707972641b8bc19a73cf8be65cd
bhumitgoyal <lifestyleprojects.rg@gmail.com>
2024-09-20 21:57:41 +0530

    Basic setup

---
+++ src/main/kotlin/com/example/vitclubapp/VitclubappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vitclubapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VitclubappApplication
+
+fun main(args: Array<String>) {
+	runApplication<VitclubappApplication>(*args)
+}
